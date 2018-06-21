216da27a0fac70fa012d4eda4e9e83709ef05e40
Gabriel Zawalski <gabrielzawalski@gmail.com>
2018-06-21 17:00:11 -0300

    texto introdução, kdd e inicio redes sociais, falta colocar referências

---
+++ TCC/configuracoes/compilador/config.bat
@@ -0,0 +1,5 @@
+"C:\Users\Gabriel Zawalski\AppData\Local\Programs\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" %1 %2 %3
+"C:\Users\Gabriel Zawalski\AppData\Local\Programs\MiKTeX 2.9\miktex\bin\x64\bibtex.exe" %4
+"C:\Users\Gabriel Zawalski\AppData\Local\Programs\MiKTeX 2.9\miktex\bin\x64\makeindex.exe" %4
+"C:\Users\Gabriel Zawalski\AppData\Local\Programs\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" %1 %2 %3
+"C:\Users\Gabriel Zawalski\AppData\Local\Programs\MiKTeX 2.9\miktex\bin\x64\pdflatex.exe" %1 %2 %3
---
+++ TCC/configuracoes/compilador/readme.txt
@@ -0,0 +1,9 @@
+CONFIGURAR NOVO TYPESET TEXWORKS----------------------------------
+
+Edit->Preferences->Typeset
+
+TCC/configuracoes/compilador/config.bat (alterar arquivo .bat com o caminho dos compiladores pdflatex, bibtex e makeindex da máquina)
+
+Arguments:	$synctexoption
+		$fullname
+		$basename
