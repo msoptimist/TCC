3a97433733a7e9e8030d41dd7f34655451ecf42f
Gabriel Zawalski <gabrielzawalski@gmail.com>
2018-09-27 22:19:12 -0300

    Experimentos inputs e outputs
    Counts, dist.neighbours, heatmaps, wss clusteres + texto resultados

--- Experimentos/R/som_input.R
+++ Experimentos/R/som_input.R
@@ -15,28 +15,28 @@ input.sc <- scale(input)
 
 #definir parametros de SOM
 #tamanho do grid
-input.grid <- somgrid(x=16, y=7, topo="hexagonal")
+input.grid <- somgrid(x=16, y=16, topo="rectangular")
 
 #executar SOM
 set.seed(100)
-input.som <- som(input.sc, grid=input.grid, rlen=100, alpha=c(0.05, 0.01))
+input.som <- som(input.sc, grid=input.grid, rlen=500, alpha=c(0.5, 0.01))
 
 #plots
-plot(input.som, type="changes", main="Progesso do treinamento")
-plot(input.som, type="counts", main="Contagem de instâncias por bloco")
-plot(input.som, type="dist.neighbours", main="Distância euclidiana entre blocos vizinhos")
+#plot(input.som, type="changes", main="Progesso do treinamento")
+#plot(input.som, type="counts", main="Contagem de instâncias por bloco")
+#plot(input.som, type="dist.neighbours", main="Distância euclidiana entre blocos vizinhos")
 
 #definicoes das cores para o heatmap
-coolBlueHotRed <- function(n, alpha = 1) {
-  rainbow(n, end=4/6, alpha=alpha)[n:1]
-}
+#coolBlueHotRed <- function(n, alpha = 1) {
+#  rainbow(n, end=4/6, alpha=alpha)[n:1]
+#}
 
 #heatmaps para todas as variaveis
-par(mfrow=c(5,2))
-for (j in 1:ncol(getCodes(input.som))){
-  plot(input.som,type="property",property=getCodes(input.som)[,j],main=names(getCodes(input.som)[1,])[j],palette.name=coolBlueHotRed)
-}
-par(mfrow=c(1,1))
+#par(mfrow=c(1,2))
+#for (j in 1:ncol(getCodes(input.som))){
+#  plot(input.som,type="property",property=getCodes(input.som)[,j],main=names(getCodes(input.som)[1,])[j],palette.name=coolBlueHotRed)
+#}
+#par(mfrow=c(1,1))
 
 #vizualizacao de numero ideal de clusteres
 #input.data <- getCodes(input.som) 
@@ -46,11 +46,11 @@ par(mfrow=c(1,1))
 #	}
 #plot(wss, main="Whitin-clusters sum of squares")
 
-wss <- fviz_nbclust(getCodes(input.som), kmeans, method = "wss", k.max = 20)
-plot(wss)
+#wss <- fviz_nbclust(getCodes(input.som), kmeans, method = "wss", k.max = 30)
+#plot(wss)
 
 #clusterizacao hierarquica
-groups = 12
+groups = 20
 input.hc = cutree(hclust(dist(getCodes(input.som))), groups)
 plot(input.som, type="codes", bgcol=rainbow(groups)[input.hc])
 add.cluster.boundaries(input.som, input.hc)
--- Experimentos/R/som_output.R
+++ Experimentos/R/som_output.R
@@ -11,40 +11,44 @@ output <- Facebook_metrics_complete[,11:22]
 #escalonar os dados das matrizes
 output.sc <- scale(output)
 
-#definir parâmetros de SOM
+#definir parametros de SOM
 #tamanho do grid
-output.grid <- somgrid(x=16, y=7, topo="hexagonal")
+output.grid <- somgrid(x=16, y=16, topo="rectangular")
 
 #executar SOM
 set.seed(100)
-output.som <- som(output.sc, grid=output.grid, rlen=100, alpha=c(0.05, 0.01))
+output.som <- som(output.sc, grid=output.grid, rlen=500, alpha=c(0.5, 0.01))
 
 #plots
-plot(output.som, type="counts")
-plot(output.som, type="dist.neighbours")
+#plot(output.som, type="counts", main="Contagem de inst�ncias por bloco")
+#plot(output.som, type="dist.neighbours", main="Dist�ncia euclidiana entre blocos vizinhos")
 
-#definições das cores para o heatmap
-coolBlueHotRed <- function(n, alpha = 1) {
-  rainbow(n, end=4/6, alpha=alpha)[n:1]
-}
+
+#definicoes das cores para o heatmap
+#coolBlueHotRed <- function(n, alpha = 1) {
+#  rainbow(n, end=4/6, alpha=alpha)[n:1]
+#}
 
 #heatmaps para todas as variáveis
-par(mfrow=c(5,2))
-for (j in 1:ncol(getCodes(output.som))){
-  plot(output.som,type="property",property=getCodes(output.som)[,j],palette.name=coolBlueHotRed,main=colnames(getCodes(output.som)[,j]),cex=0.5)
-}
-par(mfrow=c(1,1))
+#par(mfrow=c(1,2))
+#for (j in 1:ncol(getCodes(output.som))){
+#  plot(output.som,type="property",property=getCodes(output.som)[,j],palette.name=coolBlueHotRed,main=names(getCodes(output.som)[1,])[j],cex=0.5)
+#}
+#par(mfrow=c(1,1))
 
-#vizualização de numero ideal de clusteres
-output.data <- getCodes(output.som) 
-	wss <- (nrow(output.data)-1)*sum(apply(output.data,2,var)) 
-	for (i in 2:20) {
-		wss[i] <- sum(kmeans(output.data, centers=i)$withinss)
-	}
-plot(wss)
+#vizualizacao de numero ideal de clusteres
+#input.data <- getCodes(input.som) 
+#	wss <- (nrow(input.data)-1)*sum(apply(input.data,2,var)) 
+#	for (i in 2:20) {
+#		wss[i] <- sum(kmeans(input.data, centers=i)$withinss)
+#	}
+#plot(wss, main="Whitin-clusters sum of squares")
 
-#clusterização hierarquica
-groups = 9
+#wss <- fviz_nbclust(getCodes(output.som), kmeans, method = "wss", k.max = 30)
+#plot(wss)
+
+#clusterizacaoo hierarquica
+groups = 20
 output.hc = cutree(hclust(dist(getCodes(output.som))), groups)
 plot(output.som, type="codes", bgcol=rainbow(groups)[output.hc])
 add.cluster.boundaries(output.som, output.hc)
